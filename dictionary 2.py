755a8623736a32ef3df5ed88dc8d9de2372d70e9
PRIYA <priyaa58514@gmail.com>
2025-09-10 17:49:26 +0530

    Add files via upload

---
+++ dictionary 2.py
@@ -0,0 +1,13 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:\Users\Ibscai41.DGVC-MCA\AppData\Local\Programs\Python\Python313\dictionary.py
+enter the key:s1
+enter the value:python
+enter the key:s2
+enter the value:english
+enter the key:s3
+enter the value:tamil
+enter the key:s4
+enter the value:maths
+dictionary is {'s1': 'python', 's2': 'english', 's3': 'tamil', 's4': 'maths'}
